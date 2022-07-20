f1f883d17bb81c26009bac3aecac3d0d7a524c9a
WhiteBlackGoose <wbg@angouri.org>
2022-07-20 09:35:02 +0300

    initial

---
+++ WebsiteGenerator/Program.fs
@@ -0,0 +1,35 @@
+﻿open Giraffe.ViewEngine
+
+type Repo = {
+    name : string
+    lang : string
+}
+
+let myFunReposList = [
+    { name = "MyFunRepos";             lang = "F#" }
+    { name = "FStarHelloWorld";        lang = "F*" }
+    { name = "LambdaCalculusFSharp";   lang = "F#" }
+    { name = "AsmToDelegate";          lang = "C#" }
+]
+
+html [] [
+    body [] [
+        h1 [] [ Text "My Fun Repos" ]
+        ul [] [
+            for { name = name; lang = lang } in myFunReposList do
+                li [] [
+                    p [] [ Text $"Repo {name}" ]
+                    a [_href $"https://github.com/WhiteBlackGoose/{name}"] [ Text name ]
+                    p [] [
+                        Text "Made in "
+                        if lang = "F#" || lang = "C#" then
+                            Text $".NET {lang}"
+                        else
+                            Text lang
+                    ]
+                ]
+        ]        
+    ]
+]
+|> RenderView.AsString.htmlNode
+|> fun code -> System.IO.File.WriteAllText("./index.html",  code)
